b75622a4faeae15e996b15d272bd6a53c9c10093
Nimr Nimr <yujiehang@outlook.com>
2023-10-19 15:55:43 +0000

    initial commit

---
+++ languages/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+# Create your views here.
+
+
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
+
+
+class PythonPageView(TemplateView):
+    template_name = "python.html"
+
+
+class FsharpPageView(TemplateView):
+    template_name = "fsharp.html"
+
+
+class RacketPageView(TemplateView):
+    template_name = "racket.html"
